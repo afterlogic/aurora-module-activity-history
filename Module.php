81a8c61c381acb18dc0e41b57d0f77005653af5e
sash04ek <a.ovcharov@gmail.com>
2026-05-27 09:53:56 +0300

    Remove initialization of aDeniedMethodsByWebApi in Module constructor

--- Module.php
+++ Module.php
@@ -40,7 +40,6 @@ class Module extends \Aurora\System\Module\AbstractModule
         $this->subscribeEvent('CreatePublicLink::after', array($this, 'onAfterFilesCreatePublicLink'));
         $this->subscribeEvent('OpenPgpFilesWebclient::ValidatePublicLinkPassword::after', array($this, 'onAfterValidatePublicLinkPassword'));
         $this->subscribeEvent('Core::DeleteUser::after', array($this, 'onAfterDeleteUser'));
-        $this->aDeniedMethodsByWebApi = [];
     }
 
     /**
